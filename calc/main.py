b7a112a0ff9104da36db8e0a2dea6b58f785b05a
NikitaGaikwad01 <ng46@njit.edu>
2021-11-01 12:18:39 -0400

    initial commit

---
+++ calc/main.py
@@ -0,0 +1,17 @@
+""" content of calculator.py#"""
+class Calculator:
+    """ This is the Calculator class"""
+
+    result = 0
+    def get_result(self):
+        """ Get Result of Calculation"""
+        return self.result
+
+    def add_number(self, value_a):
+        """ adds number to result"""
+        self.result = self.result + value_a
+        return self.result
+    def subtract_number(self, value_a):
+        """ subtract number from result"""
+        self.result = self.result - value_a
+        return self.result
