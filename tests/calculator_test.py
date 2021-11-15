ac8e817df6db2955dca0f814ff2e43f9ce378aec
NikitaGaikwad01 <ng46@njit.edu>
2021-11-15 13:37:07 -0500

    refactor part2

---
+++ tests/calculator_test.py
@@ -0,0 +1,18 @@
+"""Testing the Calculator"""
+from calculator.calculator import Calculator
+
+def test_calculator_add():
+    """Testing the Add function of the calculator"""
+    assert Calculator.add_number(1,2) == 3
+
+def test_calculator_subtract():
+    """Testing the subtract method of the calculator"""
+    assert Calculator.subtract_number(1, 2) == -1
+
+def test_calculator_multiply():
+    """ tests multiplication of two numbers"""
+    assert Calculator.multiply_numbers(1,2) == 2
+
+def test_calculator_divide():
+    """ tests division of two numbers"""
+    assert Calculator.multiply_numbers(1,1) == 1.0
